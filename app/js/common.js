80ab0cedfb81e6872bdb53fcdb682330d5b5ca5e
Yurii <yurayurakh@mail.ru>
2017-11-24 22:43:38 +0200

    faq pagr

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,44 @@
 
+var startAnimation = (function () {
+
+    setTimeout(function () {
+
+        alert("start");
+
+    }, 2000);
+
+    return {
+        init: function () {
+            console.log('Init startAnimation');
+            startAnimation();
+        }
+    }
+})();
+
+var qestion = (function () {
+    // Hidden Answer in the Question Block
+    $('.hides-answer').hide();
+
+    // Click by Question Item
+    $('.question').click(function(){
+        $(this).toggleClass('question-open').next().stop(true,true).slideToggle(300);
+        return false;
+    });
+
+    return {
+        init: function () {
+            console.log('Init qestion');
+            qestion();
+        }
+    }
+})();
+
+(function main() {
+    $(document).ready(function () {
+        qestion.init();
+        startAnimation.init();
+    });
+})();
 $(function(){
     var canvas = document.querySelector('canvas'),
         ctx = canvas.getContext('2d'),
